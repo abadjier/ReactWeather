9a3bdbfcfa4d02a1ed0d344a7dc920d88e95a3fa
Ralitza A <raliabad@hotmail.com>
2016-11-02 19:47:32 +0200

    Prep file for Heroku

--- server.js
+++ server.js
@@ -2,12 +2,25 @@ var express = require('express');
 
 // Create our app 
 var app = express();
+// use process.env to get the environment variable 'port' that Heroku will pass us
+const PORT = process.env.PORT || 3000;
 
 // app.use lets you add functionality to your application
+
+// redirect all https traffic to http because openweathermap only operates on http
+// this is a common pattern for express middleware
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 // express.static tells us the name of the folder that will expose to the server
 app.use(express.static('public'));
 
 //start the server with app.listen - takes the port and function
-app.listen(3000, function() {
-    console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+    console.log('Express server is up on port ' + PORT);
 });
